f96b24b8bb16117ff533c42b2752677e9304dd1b
Mounika <pmounika251@gmail.com>
2021-10-13 09:36:11 +0530

    shopify mystore app

---
+++ assets/theme.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function() {
+    $("#hero").owlCarousel({
+      loop:true,
+  margin:10,
+  nav:true,
+  items: 1
+    });
+    $("#collection").owlCarousel({
+        loop:true,
+    margin:10,
+    nav:true,
+    items: 4
+      });
+      $("#women").owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+      });
+      $("#brand").owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+      });
+  }) 
+    
